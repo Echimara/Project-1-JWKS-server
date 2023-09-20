555ee2699518e0a8b2e5ba1006eb1586ce518b2e
Chimara Okeke <123244902+Echimara@users.noreply.github.com>
2023-09-19 19:03:19 -0500

    Save progress

--- RESTful_JWKS_server.py
+++ RESTful_JWKS_server.py
@@ -3,9 +3,13 @@
 # Class: CSCE3550.001
 
 # Usage: 
+'''
+---From Terminal---
+1. pip install cryptography
+'''    
 # Description: 
+ '''add later'''
 
-# Needed libs
 import flask 
 from flask import Flask, request
 from flask import jsonify
@@ -20,7 +24,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 
 app = Flask(_name_) # create flask object
 
-# Key Generation - req1
+# KEY GENERATION - req1
 # Generate an RSA key pair
 private_key = rsa.generate_private_key(
     public_exponent=65537,  
@@ -28,5 +32,23 @@ private_key = rsa.generate_private_key(
     backend=default_backend()
 )
 
-# Associate a Key ID (kid) and expiry timestamp with each key.
+# ASSOCIATE A KEY ID (KID) AND EXPIRY TIMESTAMP WITH EACH KEY. - req2
 key_id = "kid_1"
+# Get time by epoch
+curr_time = int(time.time())
+#  expiry timestamp -: 2hrs
+key_expiry = curr_time + 7200
+
+# store keys
+key_entry = {
+    "key_id": key_id,
+    "public_key": public_key,
+    "key_expiry": key_expiry
+}
+keys = [key_entry]
+
+# SERVE HTTP ON PORT 8080 - req3
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=8080)
+
+
