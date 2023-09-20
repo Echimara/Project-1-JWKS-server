484ba71977ea01a89d25632a71e13a943d75c4ff
Chimara Okeke <123244902+Echimara@users.noreply.github.com>
2023-09-19 19:14:06 -0500

    Save changes
    // needs "blackbox testing"

--- RESTful_JWKS_server.py
+++ RESTful_JWKS_server.py
@@ -32,7 +32,7 @@ private_key = rsa.generate_private_key(
     backend=default_backend()
 )
 
-# ASSOCIATE A KEY ID (KID) AND EXPIRY TIMESTAMP WITH EACH KEY. - req2
+# ASSOCIATE A KEY ID (KID) AND EXPIRY TIMESTAMP WITH EACH KEY - req2
 key_id = "kid_1"
 # Get time by epoch
 curr_time = int(time.time())
@@ -50,5 +50,45 @@ keys = [key_entry]
 # SERVE HTTP ON PORT 8080 - req3
 if _name_ == '_main_':
     app.run(host='0.0.0.0', port=8080)
+
+
+# RESTful JWKS endpoint - req4
+@app.route('/jwks', methods=['GET'])
+def jwks():
+    current_time = int(time.time())
+    if key_expiry <= current_time:
+        key_expiry = current_time
+    jwks = {
+        "keys": [
+            {
+                "kid": key_id,
+                "kty": "RSA",
+                "alg": "RS256",
+                "use": "sig",
+                "n": public_key.public_numbers().n,
+                "e": public_key.public_numbers().e,
+                "exp": key_expiry,
+            }
+        ]
+    }
+    return jsonify(jwks)
+
+# /auth endpoint defintion with HTTP method - req5
+@app.route('/auth', methods=['POST'])
+def auth():
+    # GET expired query parameter from the request
+    expired_param = request.args.get('expired')
+
+    # Parameter validation
+    if expired_param:
+        # If param is expired, generate a JWT using the expired key and expiry
+        token = generate_jwt(private_key, key_id, expired=True)
+    else:
+        # generate a JWT using the current key and expiry
+        token = generate_jwt(private_key, key_id)
+
+ #  Return an unexpired, signed JWT on the POST request
+ return jsonify({"access_token": token})
+
 
 
